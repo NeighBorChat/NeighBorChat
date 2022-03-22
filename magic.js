3fed142d0255bf270f27fc810b4e6f6d71c108b0
Cemu0 <quocanhhai@gmail.com>
2022-03-22 17:18:44 +0700

    add cripto lib

--- magic.js
+++ magic.js
@@ -3,6 +3,7 @@ import './database/MsgPks.js';
 import './database/publicPks.js';
 import 'https://unpkg.com/peerjs@1.3.1/dist/peerjs.min.js'
 import 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js'
+//cripto
 
 /*
     Run at startup
