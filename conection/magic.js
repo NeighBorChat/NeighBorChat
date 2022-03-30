2b94e5b0620bd6498ff9bf0acd4f25cceec6c486
Cemu0 <quocanhhai@gmail.com>
2022-03-30 23:57:23 +0700

    fix key

--- conection/magic.js
+++ conection/magic.js
@@ -67,13 +67,13 @@ function loadDB(){
     db.get(SYS_ID).then(function (doc) {
             console.log("load data", doc);
             // PASSPHRASE = doc.PASSPHRASE;
-            PRIVATE_KEY = doc.PUBLIC_KEY;
+            PRIVATE_KEY = doc.PRIVATE_KEY;
             PUBLIC_KEY = doc.PUBLIC_KEY;
             BITS = doc.BITS;
             MyPLD = doc.MyPLD;
             PublicListDatabase = doc.PublicListDatabase;
             holdingData = doc.holdingData;
-            CreateKey();
+            // CreateKey();
             sigUpCallBack(MyPLD.HID.name, PUBLIC_KEY);
         }).catch(function (err) {
             /* incase no DB preload */
